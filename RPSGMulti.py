36ab65b89d0a0ac7d48cd6cd0dc62f7af99ee8cc
Jcarrera007 <carrerasproductions@gmail.com>
2025-08-11 13:38:56 -0700

    added the name input and shortcuts for rock paper and scissors

--- RPSGMulti.py
+++ RPSGMulti.py
@@ -1,6 +1,7 @@
-import random
+player1 = input("Player 1 name: ")
+player2 = input("Player 2 name: ")
 
-options = ["rock", "paper", "scissors"]
+options = ["rock", "paper", "scissors", "r", "p", "s"]
 player1_score = 0
 player2_score = 0
 
@@ -9,11 +10,11 @@ print("Rock, Paper, Scissors Game")
 while player1_score < 2 and player2_score < 2:
     player1 = None
     while player1 not in options:
-        player1 = input("Player 1, choose rock, paper, scissors: ").lower()
+        player1 = input("Player1, choose 'r' for rock, 'p' for paper, or 's' for scissors: ").lower()
 
     player2 = None
     while player2 not in options:
-        player2 = input("Player 2, choose rock, paper, scissors: ").lower()
+        player2 = input("Player2, choose 'r' for rock, 'p' for paper, or 's' for scissors: ").lower()
 
     print("Player 1 chose:", player1)
     print("Player 2 chose:", player2)
@@ -21,7 +22,10 @@ while player1_score < 2 and player2_score < 2:
     wins_against = {
         "rock": "scissors",
         "paper": "rock",
-        "scissors": "paper"
+        "scissors": "paper",
+        "r": "s",
+        "s": "p",
+        "p": "r"
     }
 
     if player1 == player2:
