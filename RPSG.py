c34252f38d4bc76d1220cefbb89e5e377e4d0f3e
Jcarrera007 <carrerasproductions@gmail.com>
2025-08-11 12:55:39 -0700

    Refactor Rock, Paper, Scissors game logic for clarity and structure

--- RPSG.py
+++ RPSG.py
@@ -1,23 +1,44 @@
 import random
 
+options = ["rock", "paper", "scissors"]
+player_score = 0
+computer_score = 0
 
-print ("Rock, Paper, Scissors Game")
+print("Rock, Paper, Scissors Game")
 
-user = input("choose rock, paper, scissors: ").lower().strip()
-computer = random.choice(["rock", "paper", "scissors"])
+while player_score < 2 and computer_score < 2:
+    player = None
+    while player not in options:
+        player = input("Choose rock, paper, scissors: ").lower()
 
-print("you chose:", user)
-print("computer chose:", computer)
+    computer = random.choice(options)
 
-wins_against = {
-    "rock": "scissors",
-    "paper": "rock",
-    "scissors": "paper"
-}
-if user == computer:
-    print("It's a Tie!!")
-elif wins_against[user] == computer:
-    print("You win, Point 1 for you!!")
-    player_score += 1
+    print("You chose:", player)
+    print("Computer chose:", computer)
+
+    wins_against = {
+        "rock": "scissors",
+        "paper": "rock",
+        "scissors": "paper"
+    }
+
+    if player == computer:
+        print("It's a Tie!!")
+    elif wins_against[player] == computer:
+        print("You win, Point 1 for you!!")
+        player_score += 1
+    else:
+        print("Point for computer!!")
+        computer_score += 1
+
+    print("Score:")
+    print("You:", player_score)
+    print("Computer:", computer_score)
+    print()
+
+if player_score == 2:
+    print("Congratulations! You are the overall winner!")
 else:
-    print("You Lose!!")
+    print("Sorry! The computer is the overall winner!")
+
+print("Game Over! Thanks for playing!")
