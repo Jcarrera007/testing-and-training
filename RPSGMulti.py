3fe20004160a683cdf3f51393ba060e7b711dca5
Jcarrera007 <carrerasproductions@gmail.com>
2025-08-11 20:41:55 -0400

    Implemented the name so that it would change the player 1 to be the name chosen for it

--- RPSGMulti.py
+++ RPSGMulti.py
@@ -1,23 +1,25 @@
-player1 = input("Player 1 name: ")
-player2 = input("Player 2 name: ")
+print("Welcome to Rock ,Paper, Scissors!")
+print("Lets Begin ...")
+name1 = input("Player 1: What is your name? ")
+name2 = input("Player 2: What is your name? ")
+print("Hello " +name1 + " and " +name2)
+print(name2 + ": Close your eyes!")
 
 options = ["rock", "paper", "scissors", "r", "p", "s"]
 player1_score = 0
 player2_score = 0
 
-print("Rock, Paper, Scissors Game")
-
 while player1_score < 2 and player2_score < 2:
     player1 = None
     while player1 not in options:
-        player1 = input("Player1, choose 'r' for rock, 'p' for paper, or 's' for scissors: ").lower()
+        player1 = input(name1 + ", choose 'r' for rock, 'p' for paper, or 's' for scissors: ").lower()
 
     player2 = None
     while player2 not in options:
-        player2 = input("Player2, choose 'r' for rock, 'p' for paper, or 's' for scissors: ").lower()
+        player2 = input(name2 + ", choose 'r' for rock, 'p' for paper, or 's' for scissors: ").lower()
 
-    print("Player 1 chose:", player1)
-    print("Player 2 chose:", player2)
+    print(name1 + " chose:", player1)
+    print(name2 + " chose:", player2)
 
     wins_against = {
         "rock": "scissors",
@@ -31,20 +33,20 @@ while player1_score < 2 and player2_score < 2:
     if player1 == player2:
         print("It's a Tie!!")
     elif wins_against[player1] == player2:
-        print("Player 1 wins, Point 1 for Player 1!!")
+        print(name1 + " wins!!")
         player1_score += 1
     else:
-        print("Point for Player 2!!")
+        print(name2 + " wins!!")
         player2_score += 1
 
     print("Score:")
-    print("Player 1:", player1_score)
-    print("Player 2:", player2_score)
+    print(name1 + ":", player1_score)
+    print(name2 + ":", player2_score)
     print()
 
 if player1_score == 2:
-    print("Congratulations! Player 1 is the overall winner!")
+    print("Congratulations! " + name1 + " is the overall winner!")
 else:
-    print("Sorry! Player 2 is the overall winner!")
+    print("Sorry! " + name2 + " is the overall winner!")
 
 print("Game Over! Thanks for playing!")
